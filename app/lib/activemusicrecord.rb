de0152a3452650a5d998e47f1eb354db34e61b7c
github username <github email address>
2019-08-19 15:00:25 +0100

    Some changes in home and playlist menu

--- app/lib/activemusicrecord.rb
+++ app/lib/activemusicrecord.rb
@@ -23,7 +23,7 @@ def log_in
     else
         user = User.find_by(name: username)
     end
-    home_menu(user)
+    run_home_menu(user)                             #user.home_menu_selection
 end
 
 
@@ -34,7 +34,7 @@ def sign_up
         puts "Username already exists!"
         sign_up
     else
-        User.create(name: username)
+        User.create(name: username)         #user = User.create(name: username)
     end
     log_in
 end
@@ -54,48 +54,50 @@ def initial_logic(input)
 end
 
 ################ HOME MENU ##############################################
-def home_menu(user)
-    puts "What would you like to do?"
+def run_home_menu(user)
+    print_home_menu
+    home_menu_input = gets.chomp
+    home_menu_logic(home_menu_input, user)
+end
+
+def print_home_menu
+    puts "\nWhat would you like to do?"
     puts "1. Create new playlist"
     puts "2. Open existing playlist"
     puts "3. Just search for a song"
     puts "4. Exit"
-    input = gets.chomp
-    home_logic(input, user)
 end
 
-def home_logic(input, user)
-    case input
+def home_menu_logic(home_menu_input, user)
+    case home_menu_input
     when "1"
-        puts "Enter the name of your playlists:"
-        name = gets.chomp
-        user.create_playlist(name)
+        puts "\nEnter the name of your playlist:"
+        playlist_name = gets.chomp
+        user.create_playlist(playlist_name)
         home_menu(user)
     when "2"
-        i = 1
-        user.playlists.each do |playlist|
-            puts "#{i}. #{playlist.title}"
-            i += 1
-        end
-        puts "Please, select the playlist you want to open:"
+        puts "\nThese, are your existing playlists:"
+        user.print_playlists
+        puts "\nPlease, select the playlist you want to open:"
         playlist_number = gets.chomp.to_i
-        playlist_menu(user.playlists[playlist_number - 1])
+        selected_playlist = user.playlists[playlist_number - 1]
+        playlist_menu(selected_playlist, user)
     when "3"
-        puts "Enter the song you want to search:"
-        song = gets.chomp
-        song_search(song)
+        puts "\nEnter the song you want to search:"
+        song_name = gets.chomp
+        song_search(song_name)
     when "4"
         exit
     else
         puts "Unkown option!"
-        home_menu
+        home_menu(user)
     end
 end
 
-def song_search(song)
-    response = JSON.parse(open("https://api.deezer.com/search?q=track:#{song}").read)["data"][0...5]
+def song_search(song_name)
+    response = JSON.parse(open("https://api.deezer.com/search?q=track:#{song_name}").read)["data"][0...5]
     i = 1
-    test = response.each do |song|
+    response.each do |song|
         puts "#{i}. Title: #{song["title"]} - Artist: #{song["artist"]["name"]} - Album: #{song["album"]["title"]}"
         puts "-------------------------------------------------------"
         i += 1
@@ -104,52 +106,57 @@ end
 
 ########################## PLAYLIST MENU #############################################
 
-def playlist_menu(playlist)
+def playlist_menu(selected_playlist, user)
+    print_playlist_menu
+    playlist_menu_input = gets.chomp
+    playlist_logic(playlist_menu_input, selected_playlist, user)
+end
+
+def print_playlist_menu
     puts "What would you want to do in this playlist?"
     puts "1. Add a new song"
     puts "2. Delete an existing song"
     puts "3. Show all the songs"
-    ##### 4. return
+    puts "4. Return to Home Menu"
     puts "5. Exit"
-    input = gets.chomp
-    playlist_logic(input, playlist)
 end
 
-def playlist_logic(input, playlist)
-    case input
+def playlist_logic(playlist_menu_input, selected_playlist, user)
+    case playlist_menu_input
     when "1"
-        puts "Enter the song you want to add:"
+        puts "\nEnter the song you want to add:"
         song_name = gets.chomp
-        list = song_search(song_name)
-        puts "Please, select the song you want to add:"
+        song_list = song_search(song_name)
+        puts "\nPlease, select the song you want to add:"
         song_number = gets.chomp.to_i
-        song = list[song_number - 1]
+        song = song_list[song_number - 1]
         song_instance = Song.find_or_create_by(title: song["title"])
-        PlaylistSong.find_or_create_by(song_id: song_instance.id, playlist_id: playlist.id)
+        PlaylistSong.find_or_create_by(song_id: song_instance.id, playlist_id: selected_playlist.id)
     when "2"
         i = 1
-        playlist.songs.each do |song|
+        selected_playlist.songs.each do |song|
             puts "#{i}. Title: #{song.title}"
             i += 1
         end
-        puts "Please, select the song you want to delete:"
+        puts "\nPlease, select the song you want to delete:"
         song_number = gets.chomp.to_i
-        song_id_to_delete = playlist.songs[song_number - 1].id
-        PlaylistSong.find_by(playlist_id: playlist.id, song_id: song_id_to_delete).destroy
+        song_id_to_delete = selected_playlist.songs[song_number - 1].id
+        PlaylistSong.find_by(playlist_id: selected_playlist.id, song_id: song_id_to_delete).destroy
     when "3"
         i = 1
-        playlist.songs.each do |song|
+        selected_playlist.songs.each do |song|
             puts "#{i}. Title: #{song.title}"
             i += 1
         end
+    when "4"
+        home_menu(user)
     when "5"
         exit
     else
-        puts "Unkown option!"
-        home_menu
+        puts "\nUnkown option!"
+        playlist_menu(selected_playlist)
     end   
 end
-
 
 
 binding.pry

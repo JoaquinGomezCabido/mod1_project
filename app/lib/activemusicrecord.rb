b1fc8906e0aea097a59670ade5373a7232d517f2
github username <github email address>
2019-08-21 16:08:36 +0100

    removed commented methods

--- app/lib/activemusicrecord.rb
+++ app/lib/activemusicrecord.rb
@@ -247,17 +247,6 @@ class CLI
 
     ################  SONG SEARCH API #####################################################
 
-    # def song_search(song_name)
-    #     response = JSON.parse(open("https://api.deezer.com/search?q=#{song_name}").read)["data"][0...10]
-    #     if response
-    #         formatted_response = response.map do |song|
-    #             {title: "#{song["title"]}", artist: "#{song["artist"]["name"]}", album: "#{song["album"]["title"]}", preview: "#{song["preview"]}"}
-    #         end
-    #     else
-    #         formatted_response = nil
-    #     end
-    # end
-
     def format_song_list(song_list)
         song_list.map do |song|
             "Title: #{song[:title]} - Artist: #{song[:artist]} - Album: #{song[:album]}"
